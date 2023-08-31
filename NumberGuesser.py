b9714c16a58fdc2b881c67fc6f252dc344b9d309
Domedin <100130461+Domedin@users.noreply.github.com>
2023-08-31 13:21:23 -0400

    Improved number guesser

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -5,18 +5,20 @@ guessChange = 25
 print('This is a number guessing game welcome!')
 print('Choose a number between 1 and 100 but dont tell me!')
 
-for x in range(9):
-    if playState:
-        print(f'Is your number (greater) than, (less) than or (equal) to {guess}')
-        guessCorrectness = input()
-        if guessCorrectness == "greater":
-            guess = guess + guessChange
-        if guessCorrectness == "less":
-            guess = guess + guessChange
-        if guessCorrectness == "equal":
-            playState = False
-            print(f'YAY! I guessed your number {guess}')
+x = 0
+
+while playState:
+    print(f'Is your number (greater) than, (less) than or (equal) to {guess}')
+    guessCorrectness = input()
+    if guessCorrectness == "greater":
+        guess = guess + guessChange
+    if guessCorrectness == "less":
+        guess = guess + guessChange
+    if guessCorrectness == "equal":
+        playState = False
+        print(f'YAY! I guessed your number {guess}')
         
-        guessChange = guessChange//2
-        if guessChange <= 0:
-            guessChange = 1
+    guessChange = guessChange//2
+    if guessChange <= 0:
+        guessChange = 1
+    x = x = 1
